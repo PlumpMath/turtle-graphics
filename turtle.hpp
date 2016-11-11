34a27d9ee3c4195f906ad4b50626e171058d5bd0
Anton Te <te.anton@gmail.com>
2016-11-10 20:16:23 -0800

    Make short versions of functions

--- turtle.hpp
+++ turtle.hpp
@@ -17,3 +17,15 @@ void setX(float);
 void setXY(float, float);
 void setY(float);
 void showTurtle();
+
+inline void b(float value) { back(value); }
+inline void bg(Color value) { background(value); }
+inline void cs() { clearScreen(); }
+inline void f(float value) { forward(value); }
+inline void ht() { hideTurtle(); }
+inline void l(float value) { left(value); }
+inline void pc(Color value) { penColor(value); }
+inline void pd() { penDown(); }
+inline void pu() { penUp(); }
+inline void r(float value) { right(value); }
+inline void st() { showTurtle(); }
